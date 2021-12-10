ff8f404cc2d4ab1ee32cf5f50b5915cb5d489a74
winkyla <61644101+winkyla@users.noreply.github.com>
2021-12-10 13:33:20 +0800

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,13 @@
+function getQueryString(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = window.location.search.substr(1).match(reg);
+
+    if (r != null) {
+        return decodeURI(r[2]);
+    }
+    return null;
+}
+function goBack()
+{
+    window.history.go(-1);
+}
